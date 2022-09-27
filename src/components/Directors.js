cbbbe1faa313f1f5445355f3ba0a39ed79f5a74d
Beulah Mathenge <beulahmathenge@gmail.com>
2022-09-27 22:33:38 +0300

    Completed Assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorsList =directors.map((director)=>(
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map((mov)=> (<li key={mov}>{mov}</li>))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+    {directorsList}
+    </div>
+  );
 }
 
 export default Directors;
+// This component should render the text `Directors Page` in an `<h1>`, and make a
+// new `<div>` for each director. The `<div>` should contain the director's name
+// and a `<ul>` with a list of their movies.
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
+function Movies(movie) {
+  const moviesList =movies.map((movie)=>(
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <h2>Time: {movie.time} m</h2>
+      <ul>
+        {movie.genres.map((genre)=> (<li key={genre}> {genre}</li>))}
+      </ul>
+    </div>
+  ))
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {moviesList}
+     </div>
+  );
 }
 
 export default Movies;
+// This component should render the text `Movies Page` in an `<h1>`, and make a new
+// `<div>` for each movie. The `<div>` should contain the movie's title, time and a
+// `<ul>` with a list of its genres, each within their own `<li>`.
